692be400c5847afe5e991ea5e036e313e7327ed6
xiaho <37813368+xiaho@users.noreply.github.com>
2021-10-03 20:17:22 +0100

    Add files via upload

---
+++ javascript/title.js
@@ -0,0 +1,18 @@
+if(document.addEventListener){
+	document.addEventListener("DOMContentLoaded", function(){
+		loaded();
+	});
+} else if(document.attachEvent){
+	document.attachEvent("onreadystatechange", function(){
+		loaded();
+	});
+}
+function loaded(){
+	setInterval(loop, 300);
+}
+var x = 0;
+var titleText = [ "乡", "乡|", "乡x/", "乡xs|", "乡xsu/", "乡xs|", "乡x/", "乡|", "乡"];
+
+function loop(){
+	document.getElementsByTagName("title")[0].innerHTML = titleText[x++%titleText.length];
+}
